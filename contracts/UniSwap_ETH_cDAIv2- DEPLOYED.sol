a056847bb9502b941127e5e7f468cb42026fb627
amateur-dev <>
2019-12-22 11:25:54 +0800

    the new cDAI ZAP

--- contracts/UniSwap_ETH_cDAIv2- DEPLOYED.sol
+++ contracts/UniSwap_ETH_cDAIv2- DEPLOYED.sol
@@ -7,7 +7,7 @@ import './OpenZepplinReentrancyGuard.sol';
 
 
 ///@author DeFiZap
-///@notice this contract implements one click conversion from ETH to unipool liquidity tokens (CHAI)
+///@notice this contract implements one click conversion from ETH to unipool liquidity tokens (cDAI)
 
 interface IuniswapFactory {
     function getExchange(address token) external view returns (address exchange);
@@ -23,15 +23,18 @@ interface IuniswapExchange {
 }
 
 interface IKyberInterface {
-    function swapETHtoToken(IERC20 _TokenAddress, uint _slippageValue) external payable returns (uint);
+    function swapTokentoToken(IERC20 _srcTokenAddressERC20, IERC20 _dstTokenAddress, uint _slippageValue) external payable returns (uint);
 }
 
-interface IChaiContract {
-    function join(address dst, uint wad) external;
+interface Compound {
+    function approve ( address spender, uint256 amount ) external returns ( bool );
+    function mint ( uint256 mintAmount ) external returns ( uint256 );
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint _value) external returns (bool success);
 }
 
 
-contract UniSwap_ETH_CHAIZap is Ownable, ReentrancyGuard {
+contract UniSwap_ETH_CDAIZap is Ownable, ReentrancyGuard {
     using SafeMath for uint;
     
     // events
@@ -59,28 +62,27 @@ contract UniSwap_ETH_CHAIZap is Ownable, ReentrancyGuard {
     IuniswapFactory public UniSwapFactoryAddress = IuniswapFactory(0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95);
     IKyberInterface public KyberInterfaceAddresss;
     IERC20 public NEWDAI_TOKEN_ADDRESS = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    IChaiContract public CHAI_TOKEN_ADDRESS = IChaiContract(0x06AF07097C9Eeb7fD685c692751D5C66dB49c215);
+    Compound public COMPOUND_TOKEN_ADDRESS = Compound(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
     
     function set_KyberInterfaceAddresss(IKyberInterface _new_KyberInterfaceAddresss) public onlyOwner {
         KyberInterfaceAddresss = _new_KyberInterfaceAddresss;
     }
     
     
-    function LetsInvest(address _towhomtoissue, uint _MaxslippageValue) public payable stopInEmergency returns (uint) {
-        IERC20 ERC20TokenAddress = IERC20(address(CHAI_TOKEN_ADDRESS));
-        IuniswapExchange UniSwapExchangeContractAddress = IuniswapExchange(UniSwapFactoryAddress.getExchange(address(CHAI_TOKEN_ADDRESS)));
-        IChaiContract ChaiTokenAddress = IChaiContract(address(CHAI_TOKEN_ADDRESS));
+    function LetsInvest(address _src, address _towhomtoissue, uint _MaxslippageValue) public payable stopInEmergency returns (uint) {
+        IERC20 ERC20TokenAddress = IERC20(address(COMPOUND_TOKEN_ADDRESS));
+        IuniswapExchange UniSwapExchangeContractAddress = IuniswapExchange(UniSwapFactoryAddress.getExchange(address(COMPOUND_TOKEN_ADDRESS)));
 
         // determining the portion of the incoming ETH to be converted to the ERC20 Token
         uint conversionPortion = SafeMath.div(SafeMath.mul(msg.value, 505), 1000);
         uint non_conversionPortion = SafeMath.sub(msg.value,conversionPortion);
 
-        KyberInterfaceAddresss.swapETHtoToken.value(conversionPortion)(NEWDAI_TOKEN_ADDRESS, _MaxslippageValue);
+        KyberInterfaceAddresss.swapTokentoToken.value(conversionPortion)(IERC20(_src), NEWDAI_TOKEN_ADDRESS, _MaxslippageValue);
         uint tokenBalance = NEWDAI_TOKEN_ADDRESS.balanceOf(address(this));
-        // conversion of DAI to CHAI
+        // conversion of DAI to cDAI
         uint qty2approve = SafeMath.mul(tokenBalance, 3);
         require(NEWDAI_TOKEN_ADDRESS.approve(address(ERC20TokenAddress), qty2approve));
-        ChaiTokenAddress.join(address(this), tokenBalance);
+        COMPOUND_TOKEN_ADDRESS.mint(tokenBalance);
         uint ERC20TokenHoldings = ERC20TokenAddress.balanceOf(address(this));
         require (ERC20TokenHoldings > 0, "the conversion did not happen as planned");
         emit ERC20TokenHoldingsOnConversionDaiChai(ERC20TokenHoldings);
@@ -120,8 +122,8 @@ contract UniSwap_ETH_CHAIZap is Ownable, ReentrancyGuard {
         ERC20TokenAddress.transfer(_owner, StuckERC20Holdings);
     }
     
-    function set_new_CHAI_TokenContractAddress(address _new_CHAI_TokenContractAddress) public onlyOwner {
-        CHAI_TOKEN_ADDRESS = IChaiContract(address(_new_CHAI_TokenContractAddress));
+    function set_new_cDAI_TokenContractAddress(address _new_cDAI_TokenContractAddress) public onlyOwner {
+        COMPOUND_TOKEN_ADDRESS = Compound(address(_new_cDAI_TokenContractAddress));
         
     }
     
@@ -138,7 +140,7 @@ contract UniSwap_ETH_CHAIZap is Ownable, ReentrancyGuard {
         if (msg.sender == _owner) {
             depositETH();
         } else {
-            LetsInvest(msg.sender, 3);
+            LetsInvest(address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE), msg.sender, 3);
         }
     }
     
